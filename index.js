6c9cf71992115eeda03d041460f81bd7ca4ce190
nodbew <165175929+nodbew@users.noreply.github.com>
2024-11-30 11:08:18 +0900

    Implementing generateCalcProblem

--- index.js
+++ index.js
@@ -32,3 +32,35 @@ proceedButton.addEventListener(() => {
 function checkAnswer(userAnswer, correctAnswer) {
   return userAnswer == correctAnswer;
 }
+
+function generateCode() {
+  const [code, correctAns] = generateCalcProblem();
+  codeArea.innerHTML = code;
+  answer = correctAns;
+}
+
+function generateCalcProblem() {
+  const variables = getRandom([['a'], ['a', 'b', 'c'], ['a', 'b', 'c', 'd']]);
+  const locals = Object.fromEntries(
+    variables.map(
+      (v, i) => {
+        // The first variable should have a number
+        if (i === 0) {
+          return [v, Math.random() * 10];
+        }
+        
+        // Decide between 'a=2' and 'a=b*c'
+        if (getRandom([true, false]) && i < 1) {
+          const [operandOne, operandTwo] = [getRandom(variables[:i]), getRandom(variables[:i]);
+        } else {
+          const [operandOne, operandTwo] = [Math.random() * 10, Math.random() * 10];
+        }
+
+        // Decide between 
+      }
+    )
+  );
+
+function getRandom(options) {
+  return options[Math.floor(Math.random() * array.length)];
+}
